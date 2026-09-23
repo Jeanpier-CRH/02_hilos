cf3033a7411d7fbd8059f367e0ce502442dafa13
alexjeanpierchatachino@gmail.com <alexjeanpierchatachino@gmail.com>
2026-09-23 09:48:28 -0500

    feat: implementar codigo a main

--- hilos.c
+++ hilos.c
@@ -2,13 +2,6 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-
-void* hilo_funcion(void* arg)
-{
-    printf("Hola desde el hilo  %ld\n", (long)arg);
-    pthread_exit(NULL);
-}
-
 pthread_mutex_t mutex;
 
 void* hilo_funcion_con_mutex(void* arg)
@@ -23,13 +16,19 @@ int main ()
 {
     pthread_t hilo1, hilo2;
 
+    // Inicializar el mutex
+    pthread_mutex_init(&mutex, NULL);
+
     // Creacion de los hilos
-    pthread_create(&hilo1, NULL, hilo_funcion, (void*)1);
-    pthread_create(&hilo2, NULL, hilo_funcion, (void*)2);
+    pthread_create(&hilo1, NULL, hilo_funcion_con_mutex, (void*)1);
+    pthread_create(&hilo2, NULL, hilo_funcion_con_mutex, (void*)2);
 
     // Esperar a que los hilos terminen su ejecucion
     pthread_join(hilo1, NULL);
     pthread_join(hilo2, NULL);
+
+    // Destruir el mutex
+    pthread_mutex_destroy(&mutex);
 
     printf("Finalizo la ejecucion del programa principal\n");
     return 0;
